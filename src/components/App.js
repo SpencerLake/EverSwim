d092a3e157315ac91448d4e6d471b3feae94c74c
Spencer-Lake <spencer.m.lake11@gmail.com>
2022-01-18 08:55:57 -0500

    attempting to fix bug with reinstalling node_modules

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
 import './App.css'
+import React from 'react'
 import { initializeApp } from 'firebase/app'
 import { getAnalytics } from 'firebase/analytics'
 import Signup from './Signup.js'
 import Test from './Test.js'
+import AuthProvider from './contexts/AuthContext'
+import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <Test />
-    // <Signup />
+    <div className='app'>
+      <main>
+        <AuthProvider>
+          <Routes>
+            <Route path='/signup' element={<Test />} />
+            {/* <Route path="/login" component={Login} /> */}
+            {/* <Route path="/update-profile" component={UpdateProfile} /> */}
+            {/* <Route path="/home" component={Home} /> */}
+            {/* <PrivateRoute  path="/profile" component={ProfilePage} /> */}
+          </Routes>
+        </AuthProvider>
+      </main>
+    </div>
   )
 }
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase/compat/app'
 import 'firebase/firestore'
-import 'firebase/auth'
+require('firebase/auth')
 
 const app = firebase.initializeApp({
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
